8ea22b0908010569130c31dedf11d91c0e0de154
Joseph Serrato <joseph.serrato@gmail.com>
2019-04-24 16:03:38 -0700

    fixed server problem

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,15 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-connection = mysql.createConnection(process.env.JAWSDB_URL);
+  console.log('wow')
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+  console.log('wow')
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'password',
+    port: 8889,
     database: 'burgers_db'
   });
 };
--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,5 @@
-DROP DATABASE IF EXISTS burgers_db;
 
-CREATE DATABASE burgers_db;
-
-USE burgers_db;
+USE ecs6mvtgm0eefxmr;
 
 CREATE TABLE burgers
 (
